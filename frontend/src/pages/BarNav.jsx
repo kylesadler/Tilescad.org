4af5905319328f0227f52bb6758319b666fd3ae5
Kyle Sadler <kylesadler5@gmail.com>
2022-12-08 19:32:36 -0600

    update navbar

--- frontend/src/pages/BarNav.jsx
+++ frontend/src/pages/BarNav.jsx
@@ -7,14 +7,12 @@ function BarNav() {
   return (
     <div className="Navigation">
       <Navbar
-        // bg="light"
         variant="light"
         sticky="top"
         expand="sm"
         collapseOnSelect
         style={{
           padding: "10px 20px",
-          // borderBottom: "1px solid gray",
           boxShadow: "0px 1px 5px gray",
           backgroundColor: "#ffffff82",
         }}
@@ -28,7 +26,6 @@ function BarNav() {
           <Nav>
             <Nav.Link href="design">Design</Nav.Link>
             <Nav.Link href="smiley-face">Demo</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
